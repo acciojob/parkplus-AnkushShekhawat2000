b71b0ccb98744ebd540f63e8d88098cef2cc3d5d
Ankush Shekhawat <ankush75056@gmail.com>
2023-09-27 19:47:14 +0530

    again try

---
+++ src/main/java/com/driver/model/User.java
@@ -0,0 +1,76 @@
+package com.driver.model;
+
+import org.hibernate.engine.internal.Cascade;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    private String password;
+
+
+    @OneToMany(mappedBy = "user", cascade =  CascadeType.ALL)
+    List<Reservation> reservationList;
+
+    public User(int id, String name,String phoneNumber, String password, List<Reservation> reservationList) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.reservationList = reservationList;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Reservation> getReservationList() {
+        return reservationList;
+    }
+
+    public void setReservationList(List<Reservation> reservationList) {
+        this.reservationList = reservationList;
+    }
+}
